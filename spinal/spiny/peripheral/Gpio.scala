373c4fc9bdfcac8132cec0656cf86f6172c1da0a
Craig Bishop <craig@craigjb.com>
2025-12-16 13:04:28 -0700

    Rename peripherals package.

--- spinal/spiny/peripheral/Gpio.scala
+++ spinal/spiny/peripheral/Gpio.scala
@@ -29,7 +29,7 @@
 ** OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE **
 ** USE OR OTHER DEALINGS IN THE SOFTWARE.                                    */
 
-package spiny.peripherals
+package spiny.peripheral
 
 import spinal.core._
 import spinal.lib._
