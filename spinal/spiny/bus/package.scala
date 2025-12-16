81b60d36274beb5138272f7e6d335570bc43863c
Craig Bishop <craig@craigjb.com>
2025-12-15 22:38:50 -0700

    Added mappedSize helper to BusIf

---
+++ spinal/spiny/bus/package.scala
@@ -0,0 +1,53 @@
+/*                           /$$                                             **
+**                          |__/                                             **
+**        /$$$$$$$  /$$$$$$  /$$ /$$$$$$$  /$$   /$$                         **
+**       /$$_____/ /$$__  $$| $$| $$__  $$| $$  | $$                         **
+**      |  $$$$$$ | $$  \ $$| $$| $$  \ $$| $$  | $$   (c) Craig J Bishop    **
+**       \____  $$| $$  | $$| $$| $$  | $$| $$  | $$   All rights reserved   **
+**       /$$$$$$$/| $$$$$$$/| $$| $$  | $$|  $$$$$$$                         **
+**      |_______/ | $$____/ |__/|__/  |__/ \____  $$   MIT License           **
+**                | $$                     /$$  | $$                         **
+**                | $$                    |  $$$$$$/                         **
+**                |__/                     \______/                          **
+**                                                                           **
+** Permission is hereby granted, free of charge, to any person obtaining a   ** 
+** copy of this software and associated documentation files (the             **
+** "Software"), to deal in the Software without restriction, including       **
+** without limitation the rights to use, copy, modify, merge, publish,       **
+** distribute, sublicense, and/or sell copies of the Software, and to permit **
+** persons to whom the Software is furnished to do so, subject to the        **
+** following conditions:                                                     **
+**                                                                           **
+** The above copyright notice and this permission notice shall be included   **
+** in all copies or substantial portions of the Software.                    **
+**                                                                           **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   **
+** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                **
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN **
+** NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  **
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     **
+** OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE **
+** USE OR OTHER DEALINGS IN THE SOFTWARE.                                    */
+
+package spiny
+
+import spinal.core._
+import spinal.lib.bus.regif._
+
+package object bus {
+  implicit class BusIfRich(val busIf: BusIf) extends AnyVal {
+    /** Calculates total size in bytes required by the memory map
+     *
+     * Iterates through all slices (Regs, RAMs, FIFOs) to find max address.
+     */
+    def getMappedSize: BigInt = {
+      // if empty, default to one word width
+      if (busIf.slices.isEmpty) {
+        return BigInt(busIf.busByteWidth)
+      } 
+
+      val endAddresses = busIf.slices.map(slice => slice.addr + slice.size)
+      endAddresses.max
+    }
+  }
+}
