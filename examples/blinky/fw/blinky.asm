81618b5a54f4fb18199e7cbc91f361cb4fa50f70
Craig Bishop <craig@craigjb.com>
2026-01-02 15:00:05 -0700

    Skip off switches in blinky example fw

--- examples/blinky/fw/blinky.asm
+++ examples/blinky/fw/blinky.asm
@@ -1,30 +1,33 @@
 .section .text
-  .globl _start
+.globl _start
 _start:
       li x5, 0x1
-      li x28, 0x8000
       li x6, 0x10000000
+      li x28, 0x8000      
 
 outer:
-      li x7, 0x8 # 0x80000
+      li x7, 0x100000
 
 loop0:
       sub x7, x7, x5
       bne x7, x0, loop0
       lw x26, 0x100(x6)
+      beq x26, x0, clear_leds
 
-loop1:
+find_next_active:
+      bne x28, x0, check_bit
+      li x28, 0x8000
+
+check_bit:
       and x27, x26, x28
       srli x28, x28, 1
       bne x27, x0, show_led
-      beq x28, x0, reset
-      j loop1
+      j find_next_active
 
 show_led:
       sw x27, 0(x6)
-      beq x28, x0, reset
       j outer
 
-reset:
-      li x28, 0x8000
+clear_leds:
+      sw x0, 0(x6)
       j outer
