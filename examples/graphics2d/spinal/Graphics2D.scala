613df3d84a603fc0713f401f9145dd968175161a
Craig Bishop <craig@craigjb.com>
2026-05-05 21:12:31 -0700

    Larger tileset in Graphics2D example

--- examples/graphics2d/spinal/Graphics2D.scala
+++ examples/graphics2d/spinal/Graphics2D.scala
@@ -39,7 +39,42 @@ import spiny.Utils.readBytesFromFile
 import spiny.graphics._
 import spiny.hdmi._
 
+object Graphics2D {
+  val DisplayMode = VideoMode.CvtRb.CvtRb540p60Hz
+
+  val TileSize = 16
+  val TileSetData = readBytesFromFile("data/tileset.bin")
+  assert(TileSetData.length % (TileSize * TileSize) == 0)
+  val TileSetSize = TileSetData.length / TileSize / TileSize
+
+  val MapWidth = 128
+  val MapHeight = 128
+  assert(TileSetSize <= 256)
+
+  val MapData = {
+    val rng = new scala.util.Random(42)
+    (0 until (MapWidth * MapHeight))
+      .map(_ => rng.nextInt(TileSetSize))
+      .toSeq
+  }
+  val MapConfig = TileMapConfig(
+    tileSize = TileSize,
+    tileSetSize = TileSetSize,
+    mapWidth = MapWidth,
+    mapHeight = MapHeight,
+    viewportWidth = DisplayMode.hActive >> 1,
+    viewportHeight = DisplayMode.vActive >> 1
+  )
+
+  val PaletteData = readBytesFromFile("data/palette.bin")
+    .grouped(3)
+    .map(rgb => (rgb(0), rgb(1), rgb(2)))
+    .toSeq
+}
+
 class Graphics2D extends Component {
+  import Graphics2D._
+
   val io = new Bundle {
     val SYS_CLK = in(Bool())
     val CPU_RESET_N = in(Bool())
@@ -54,62 +89,42 @@ class Graphics2D extends Component {
     frequency = FixedFrequency(100 MHz)
   )
 
-  val videoMode = VideoMode.CvtRb.CvtRb540p60Hz
-
   val clockGen = inputClkDomain on ClockGen()
   val pixelClk5XDomain = clockGen.request(
-    videoMode.pixelClkFreq * 5,
-    tolerance = videoMode.pixelClkTolerance
+    DisplayMode.pixelClkFreq * 5,
+    tolerance = DisplayMode.pixelClkTolerance
   )
   val pixelClkDomain = clockGen.requestDivided(pixelClk5XDomain, divisor = 5)
   clockGen.build()
 
   pixelClkDomain on {
-    val timingGen = VideoTimingGen.static(videoMode)
+    val timingGen = VideoTimingGen.static(DisplayMode)
     val vSyncPulse = timingGen.io.timing.vSyncActive.rise()
 
-    val tileMapConfig = TileMapConfig(
-      tileSize = 16,
-      tileSetSize = 4,
-      mapWidth = 64,
-      mapHeight = 64,
-      viewportWidth = videoMode.hActive / 2,
-      viewportHeight = videoMode.vActive / 2
-    )
-
-    val tileMapPos = TileMapPosition(tileMapConfig)
+    val tileMapPos = TileMapPosition(MapConfig)
     tileMapPos.x := (timingGen.io.x >> 1).resized
     tileMapPos.y := (timingGen.io.y >> 1).resized
     tileMapPos.scrollX := Counter(
-      tileMapConfig.mapWidthPixels,
+      MapConfig.mapWidthPixels,
       inc = vSyncPulse
     ).intoSInt.resized
-    tileMapPos.scrollY := 0
+    tileMapPos.scrollY := Counter(
+      MapConfig.mapHeightPixels,
+      inc = vSyncPulse
+    ).intoSInt.resized
 
-    val tileMap = TileMap(tileMapConfig, PalettePixel(8 bits))
+    val tileMap = TileMap(MapConfig, PalettePixel(8 bits))
     tileMap.io.position.valid := timingGen.io.timing.videoActive
     tileMap.io.position.payload := tileMapPos
-    tileMap.tileMem.init(
-      readBytesFromFile("data/tileset.bin")
-        .map(c => PalettePixel(c, 8 bits))
-    )
-    val rng = new scala.util.Random(42)
-    val mapData = (0 until (tileMapConfig.mapWidth * tileMapConfig.mapHeight))
-      .map(_ => tileMapConfig.tile(rng.nextInt(4)))
-      .toSeq
-    tileMap.mapMem.init(mapData)
+    tileMap.tileMem.init(TileSetData.map(c => PalettePixel(c, 8 bits)))
+    tileMap.mapMem.init(MapData.map(i => U(i, log2Up(TileSetSize) bits)))
 
     val tilePixel = Mux(
       tileMap.io.pixels.valid,
       tileMap.io.pixels.payload,
       PalettePixel(0, 8 bits)
     )
-    val palette = Mem(
-      readBytesFromFile("data/palette.bin")
-        .grouped(3)
-        .map(rgb => RgbPixel(rgb(0), rgb(1), rgb(2)))
-        .toSeq
-    )
+    val palette = Mem(PaletteData.map { case (r, g, b) => RgbPixel(r, g, b) })
     val rgbPixel = palette.readSync(tilePixel)
 
     val timing = Delay(timingGen.io.timing, TileMap.Latency + 1)
