f1bc01158c3e87d255dd39bd13cc12f63579ad10
Craig Bishop <craig@craigjb.com>
2026-05-06 11:06:01 -0700

    Graphics2D example refactor for sim

--- examples/graphics2d/spinal/Graphics2D.scala
+++ examples/graphics2d/spinal/Graphics2D.scala
@@ -31,15 +31,21 @@
 
 package spiny.examples.graphics2d
 
+import java.io.File
+import java.io.OutputStream
+import org.rogach.scallop._
+import scala.sys.process._
+
 import spinal.core._
+import spinal.core.sim._
 import spinal.lib._
 
-import spiny.ClockGen
-import spiny.Utils.readBytesFromFile
+import spiny._
+import spiny.Utils._
 import spiny.graphics._
 import spiny.hdmi._
 
-object Graphics2D {
+object GraphicsDemo {
   val DisplayMode = VideoMode.CvtRb.CvtRb540p60Hz
 
   val TileSize = 16
@@ -72,9 +78,50 @@ object Graphics2D {
     .toSeq
 }
 
-class Graphics2D extends Component {
-  import Graphics2D._
+case class GraphicsDemo() extends Component {
+  import GraphicsDemo._
 
+  val io = new Bundle {
+    val timing = out(VideoTiming())
+    val pixel = out(RgbPixel())
+  }
+
+  val timingGen = VideoTimingGen.static(DisplayMode)
+  val vSyncPulse = timingGen.io.timing.vSyncActive.rise()
+
+  val tileMapPos = TileMapPosition(MapConfig)
+  tileMapPos.x := (timingGen.io.x >> 1).resized
+  tileMapPos.y := (timingGen.io.y >> 1).resized
+  tileMapPos.scrollX := Counter(
+    MapConfig.mapWidthPixels,
+    inc = vSyncPulse
+  ).intoSInt.resized
+  tileMapPos.scrollY := Counter(
+    MapConfig.mapHeightPixels,
+    inc = vSyncPulse
+  ).intoSInt.resized
+
+  val tileMap = TileMap(MapConfig, PalettePixel(8 bits))
+  tileMap.io.position.valid := timingGen.io.timing.videoActive
+  tileMap.io.position.payload := tileMapPos
+  tileMap.tileMem.init(TileSetData.map(c => PalettePixel(c, 8 bits)))
+  tileMap.mapMem.init(MapData.map(i => U(i, log2Up(TileSetSize) bits)))
+
+  val tilePixel = Mux(
+    tileMap.io.pixels.valid,
+    tileMap.io.pixels.payload,
+    PalettePixel(0, 8 bits)
+  )
+  val palette = Mem(PaletteData.map { case (r, g, b) => RgbPixel(r, g, b) })
+  io.pixel := palette.readSync(tilePixel)
+  io.timing := Delay(
+    timingGen.io.timing,
+    cycleCount = TileMap.Latency + 1,
+    init = VideoTiming.inactive()
+  )
+}
+
+class Graphics2D extends Component {
   val io = new Bundle {
     val SYS_CLK = in(Bool())
     val CPU_RESET_N = in(Bool())
@@ -89,49 +136,19 @@ class Graphics2D extends Component {
     frequency = FixedFrequency(100 MHz)
   )
 
-  val clockGen = inputClkDomain on ClockGen()
+  val clockGen = inputClkDomain on ClockGen(bandwidth = ClockGenBandwidth.High)
   val pixelClk5XDomain = clockGen.request(
-    DisplayMode.pixelClkFreq * 5,
-    tolerance = DisplayMode.pixelClkTolerance
+    GraphicsDemo.DisplayMode.pixelClkFreq * 5,
+    tolerance = GraphicsDemo.DisplayMode.pixelClkTolerance
   )
   val pixelClkDomain = clockGen.requestDivided(pixelClk5XDomain, divisor = 5)
   clockGen.build()
 
   pixelClkDomain on {
-    val timingGen = VideoTimingGen.static(DisplayMode)
-    val vSyncPulse = timingGen.io.timing.vSyncActive.rise()
-
-    val tileMapPos = TileMapPosition(MapConfig)
-    tileMapPos.x := (timingGen.io.x >> 1).resized
-    tileMapPos.y := (timingGen.io.y >> 1).resized
-    tileMapPos.scrollX := Counter(
-      MapConfig.mapWidthPixels,
-      inc = vSyncPulse
-    ).intoSInt.resized
-    tileMapPos.scrollY := Counter(
-      MapConfig.mapHeightPixels,
-      inc = vSyncPulse
-    ).intoSInt.resized
-
-    val tileMap = TileMap(MapConfig, PalettePixel(8 bits))
-    tileMap.io.position.valid := timingGen.io.timing.videoActive
-    tileMap.io.position.payload := tileMapPos
-    tileMap.tileMem.init(TileSetData.map(c => PalettePixel(c, 8 bits)))
-    tileMap.mapMem.init(MapData.map(i => U(i, log2Up(TileSetSize) bits)))
-
-    val tilePixel = Mux(
-      tileMap.io.pixels.valid,
-      tileMap.io.pixels.payload,
-      PalettePixel(0, 8 bits)
-    )
-    val palette = Mem(PaletteData.map { case (r, g, b) => RgbPixel(r, g, b) })
-    val rgbPixel = palette.readSync(tilePixel)
-
-    val timing = Delay(timingGen.io.timing, TileMap.Latency + 1)
-
+    val demo = GraphicsDemo()
     val hdmiTx = HdmiTx(pixelClk5XDomain)
-    hdmiTx.io.timing := timing
-    hdmiTx.io.pixel := rgbPixel
+    hdmiTx.io.timing := demo.io.timing
+    hdmiTx.io.pixel := demo.io.pixel
     io.HDMI := hdmiTx.io.hdmi
   }
 }
@@ -141,4 +158,109 @@ object TopLevelVerilog extends App {
     targetDirectory = "target/spinal",
     inlineRom = true
   ).generateVerilog(new Graphics2D())
+}
+
+class VideoDump(videoMode: VideoMode, outputPath: String) {
+  var stdInStream: OutputStream = null
+  val processIo = new ProcessIO(
+    in => stdInStream = in, 
+    out => scala.io.Source.fromInputStream(out).getLines().foreach(println),
+    err => scala.io.Source.fromInputStream(err).getLines().foreach(println)
+  )
+
+  val ffmpegCmd = Seq(
+    "ffmpeg",
+    "-y",                  // Overwrite output file
+    "-f", "rawvideo",      // Raw binary data in
+    "-vcodec", "rawvideo",
+    "-s", s"${videoMode.hActive}x${videoMode.vActive}",
+    "-pix_fmt", "rgb24",   // 3 bytes per pixel (R, G, B)
+    "-r", videoMode.frameRate.toDouble.round.toString,
+    "-i", "-",             // Read from stdin
+    "-c:v", "libx264",
+    "-preset", "medium",
+    "-pix_fmt", "yuv420p",  // Standard pixel format for mp4 players
+    outputPath
+  )
+  println(s"ffmpeg command: ${ffmpegCmd.mkString(" ")}")
+
+  val process = Process(ffmpegCmd).run(processIo)
+  // wait for the ProcessIO thread to capture the stdin stream
+  while(stdInStream == null) Thread.sleep(10)
+
+  def writeFrame(buffer: Array[Byte]) {
+    stdInStream.write(buffer)
+  }
+
+  def close() {
+    println("Flushing ffmpeg stdin")
+    stdInStream.flush()
+    println("Closing ffmpeg stdin")
+    stdInStream.close()
+    println("Exiting ffmpeg")
+    process.exitValue()
+    println("Done with ffmpeg")
+  }
+}
+
+object TopLevelSim extends App {
+  import org.rogach.scallop._
+
+  object Conf extends ScallopConf(args) {
+    val frames = opt[Int](default = Some(60))
+    val outputVideoPath = opt[File](
+      default = Some(new File("simWorkspace/GraphicsDemo/sim.mp4"))
+    )
+    val withWave = toggle("wave", default = Some(false))
+    verify()
+  }
+  println(f"[Graphics2D] TopLevelSim.Conf: ${Conf.summary}")
+
+  val videoDump = new VideoDump(
+    videoMode = GraphicsDemo.DisplayMode,
+    outputPath = Conf.outputVideoPath().getAbsolutePath()
+  )
+
+  val simConfig = if(Conf.withWave()) {
+    SimConfig.withWave
+  } else {
+    SimConfig
+  }
+
+  simConfig
+    .withConfig(SpinalConfig(
+      defaultClockDomainFrequency = FixedFrequency(
+        GraphicsDemo.DisplayMode.pixelClkFreq)))
+    .allOptimisation
+    .compile(GraphicsDemo())
+    .doSim { dut =>
+      val cd = dut.clockDomain
+      cd.forkStimulus()
+
+      val frameWidth = GraphicsDemo.DisplayMode.hActive
+      val frameHeight = GraphicsDemo.DisplayMode.vActive
+      val frameBuffer = new Array[Byte](frameWidth * frameHeight * 3)
+      var framesCaptured = 0
+      var pixelIndex = 0
+      var flushed = false
+      while(framesCaptured < Conf.frames()) {
+        cd.waitSampling()
+        if(dut.io.timing.videoActive.toBoolean) {
+          flushed = false
+          frameBuffer(pixelIndex) = dut.io.pixel.r.toInt.toByte
+          frameBuffer(pixelIndex + 1) = dut.io.pixel.g.toInt.toByte
+          frameBuffer(pixelIndex + 2) = dut.io.pixel.b.toInt.toByte
+          pixelIndex += 3
+        } else if(!flushed && dut.io.timing.vSyncActive.toBoolean) {
+          videoDump.writeFrame(frameBuffer)
+          flushed = true
+          framesCaptured += 1
+          pixelIndex = 0
+          println(s"Captured frame ${framesCaptured} of ${Conf.frames()}")
+          cd.waitActiveEdgeWhere(!dut.io.timing.vSyncActive.toBoolean)
+        }
+      }
+      println("Simulation complete")
+      videoDump.close()
+    }
 }
