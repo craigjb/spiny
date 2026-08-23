8dee151f9d32aacc4d46be15b8e3017cc45de6b1
Craig Bishop <craig@craigjb.com>
2026-08-22 21:11:24 -0700

    AuxPhy to integrate tx + rx

---
+++ spinal/spiny/displayport/AuxPhy.scala
@@ -0,0 +1,58 @@
+/*                           /$$                                             **
+**                          |__/                                             **
+**        /$$$$$$$  /$$$$$$  /$$ /$$$$$$$  /$$   /$$                         **
+**       /$$_____/ /$$__  $$| $$| $$__  $$| $$  | $$                         **
+**      |  $$$$$$ | $$  \ $$| $$| $$  \ $$| $$  | $$   (c) Craig J Bishop    **
+**       \____  $$| $$  | $$| $$| $$  | $$| $$  | $$   All rights reserved   **
+**       /$$$$$$$/| $$$$$$$/| $$| $$  | $$|  $$$$$$$                         **
+**      |_______/ | $$____/ |__/|__/  |__/ \____  $$   MIT License           **
+**                | $$                     /$$  | $$                         **
+**                | $$                    |  $$$$$$/                         **
+**                |__/                     \______/                          **
+**                                                                           **
+** Permission is hereby granted, free of charge, to any person obtaining a   **
+** copy of this software and associated documentation files (the             **
+** "Software"), to deal in the Software without restriction, including       **
+** without limitation the rights to use, copy, modify, merge, publish,       **
+** distribute, sublicense, and/or sell copies of the Software, and to permit **
+** persons to whom the Software is furnished to do so, subject to the        **
+** following conditions:                                                     **
+**                                                                           **
+** The above copyright notice and this permission notice shall be included   **
+** in all copies or substantial portions of the Software.                    **
+**                                                                           **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   **
+** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                **
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN **
+** NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  **
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     **
+** OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE **
+** USE OR OTHER DEALINGS IN THE SOFTWARE.                                    */
+
+package spiny.displayport
+
+import spinal.core._
+import spinal.lib._
+import spinal.lib.io._
+
+case class AuxPhy(
+  dataRate: HertzNumber = 1 MHz,
+  tolerance: BigDecimal = 0.15
+) extends Component {
+  val io = new Bundle {
+    val aux = master(TriState(Bool()))
+    val txData = slave(Stream(Fragment(Bits(8 bits))))
+    val rxData = master(Flow(Fragment(Bits(8 bits))))
+  }
+
+  val tx = AuxTx(dataRate = dataRate)
+  io.txData >> tx.io.data
+  io.aux.write := tx.io.write
+  io.aux.writeEnable := tx.io.writeEnable
+
+  val rx = AuxRx(dataRate = dataRate, tolerance = tolerance)
+  rx.io.read := io.aux.read
+  rx.io.readEnable := !tx.io.writeEnable
+  rx.io.data >> io.rxData
+}
+
