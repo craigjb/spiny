ec1eb91feaa538fc00c0da0a8b0e5f2ab7901d2e
Craig Bishop <craig@craigjb.com>
2026-08-30 14:16:56 -0700

    IOBUFDS helper for TriState[Bool]

---
+++ spinal/spiny/blackbox/package.scala
@@ -0,0 +1,51 @@
+/*                           /$$                                             **
+**                          |__/                                             **
+**        /$$$$$$$  /$$$$$$  /$$ /$$$$$$$  /$$   /$$                         **
+**       /$$_____/ /$$__  $$| $$| $$__  $$| $$  | $$                         **
+**      |  $$$$$$ | $$  \ $$| $$| $$  \ $$| $$  | $$   (c) Craig J Bishop    **
+**       \____  $$| $$  | $$| $$| $$  | $$| $$  | $$   All rights reserved   **
+**       /$$$$$$$/| $$$$$$$/| $$| $$  | $$|  $$$$$$$                         **
+**      |_______/ | $$____/ |__/|__/  |__/ \____  $$   MIT License           **
+**                | $$                     /$$  | $$                         **
+**                | $$                    |  $$$$$$/                         **
+**                |__/                     \______/                          **
+**                                                                           **
+** Permission is hereby granted, free of charge, to any person obtaining a   **
+** copy of this software and associated documentation files (the             **
+** "Software"), to deal in the Software without restriction, including       **
+** without limitation the rights to use, copy, modify, merge, publish,       **
+** distribute, sublicense, and/or sell copies of the Software, and to permit **
+** persons to whom the Software is furnished to do so, subject to the        **
+** following conditions:                                                     **
+**                                                                           **
+** The above copyright notice and this permission notice shall be included   **
+** in all copies or substantial portions of the Software.                    **
+**                                                                           **
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS   **
+** OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF                **
+** MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN **
+** NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,  **
+** DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR     **
+** OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE **
+** USE OR OTHER DEALINGS IN THE SOFTWARE.                                    */
+
+package spiny.blackbox
+
+import spinal.core._
+import spinal.lib.blackbox.xilinx.s7.IOBUFDS
+import spinal.lib.io.TriState
+
+package object xilinx {
+  implicit class IOBUFDSRich(val factory: IOBUFDS.type) extends AnyVal {
+    /** Buffers a tri-state onto a differential pad pair */
+    def on(triState: TriState[Bool], p: Bool, n: Bool): IOBUFDS = {
+      val buffer = IOBUFDS()
+      buffer.I := triState.write
+      buffer.T := !triState.writeEnable
+      triState.read := buffer.O
+      p := buffer.IO
+      n := buffer.IOB
+      buffer
+    }
+  }
+}
